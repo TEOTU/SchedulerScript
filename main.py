dc9cfe9c2d92139ab464abdfd6ff29e5830ad1e1
Krasavchik <mhailtnko@gmail.com>
2024-02-01 17:32:24 +0100

    remaking the whole functionality

--- main.py
+++ main.py
@@ -10,32 +10,11 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "1yca1wLBj0vRrgbG6x_lg5IQtNqH3GAfE36mJWIefAlU"
-FIRST_TOPIC_RANGE, FIRST_ROW_RANGE = 2, 1  # All the topics are stored from the row 1(which is second) and value 1(also)
+SPREADSHEET_ID = "1yca1wLBj0vRrgbG6x_lg5IQtNqH3GAfE36mJWIefAlU"
+RANGE = 'Studying!E1:I1009'  # All the topics are stored from the row 1(which is second) and value 1(also)
 CREDS = None
 SHEET_NUM = 2  # The number of the sheet in a spreadsheet
 DATA_ENTRY = 0
-
-
-def cache_read():
-    # read last cell checked
-    with open('cache.txt', 'r') as file:
-        return file.read()
-
-
-def cache_write(last: str):
-    # write the last checked cell
-    with open('cache.txt', 'w') as file:
-        file.write(last)
-
-
-def last_checked_row():
-    last_row = cache_read()
-    if not cache_read():  # if empty
-        last_row = 1
-        return last_row
-    else:
-        return last_row
 
 
 def get_sheet_info():
@@ -46,30 +25,20 @@ def get_sheet_info():
     sheet = service.spreadsheets()
     result = (
         sheet
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, includeGridData=True)
+        .values()
+        .get(spreadsheetId=SPREADSHEET_ID, range=RANGE)
         .execute()
     )
     return result
 
 
-def get_color(row):
+def get_info():
     result = get_sheet_info()
     values = result.get('values', [])
     if not values:
         print("No data found.")
     else:
-        cell_data = result['sheets'][SHEET_NUM]['data'][DATA_ENTRY]['rowData'][row]['values'][FIRST_TOPIC_RANGE]
-        return cell_data.get('effectiveFormat', {}).get("backgroundColor", {})  # return color
-
-
-def sheet_len():
-    result = get_sheet_info()
-    values = result.get('values', [])
-    if not values:
-        print('No data found')
-        return
-    else:
-        return len(values)
+        return values
 
 
 def connect():
@@ -92,24 +61,10 @@ def connect():
         with open("token.json", "w") as token:
             token.write(CREDS.to_json())
 
-
-def set_intervals():
-    last_row = last_checked_row()
-    for row in range(last_row, sheet_len()):
-        color_key = get_color(row).key()
-        if color_key == 'green':
-            print('green')
-        if color_key == 'orange':
-            print('orange')
-        if color_key == 'red':
-            print('red')
-        if color_key == 'black':
-            print('black')
-
 def main():
     try:
         connect()
-        set_intervals()
+        print(get_info())
     except HttpError as err:
         print(err)
 
